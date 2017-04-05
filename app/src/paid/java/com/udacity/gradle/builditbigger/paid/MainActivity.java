8d8a0e5944dd0a5c98a06e540ee49b8247909cee
yehiahd <yehia_hd@yahoo.com>
2017-04-05 04:46:32 +0200

    adding Paid Flavor

--- app/src/paid/java/com/udacity/gradle/builditbigger/paid/MainActivity.java
+++ app/src/paid/java/com/udacity/gradle/builditbigger/paid/MainActivity.java
@@ -1,4 +1,4 @@
-package com.udacity.gradle.builditbigger.free;
+package com.udacity.gradle.builditbigger.paid;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,9 +9,6 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.fci.yehiahd.jokelibrary.JokeActivity;
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
-import com.udacity.gradle.builditbigger.BuildConfig;
 import com.udacity.gradle.builditbigger.R;
 import com.udacity.gradle.builditbigger.RxEndPoint;
 
@@ -25,21 +22,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initAd();
-    }
-
-    private void initAd() {
-        if (BuildConfig.FLAVOR.equals("free")){
-            AdView mAdView = (AdView) findViewById(R.id.adView);
-            // Create an ad request. Check logcat output for the hashed device ID to
-            // get test ads on a physical device. e.g.
-            // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
-            AdRequest adRequest = new AdRequest.Builder()
-                    .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
-                    .build();
-            mAdView.loadAd(adRequest);
-        }
-
     }
 
     @Override
