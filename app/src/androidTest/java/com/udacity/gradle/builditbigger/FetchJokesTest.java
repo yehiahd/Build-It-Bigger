645d7d4c908a8771351b422688cf63335cf22a6a
yehiahd <yehia_hd@yahoo.com>
2017-04-05 02:41:12 +0200

    remove Fragment and work with activity

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/FetchJokesTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/FetchJokesTest.java
@@ -1,8 +1,28 @@
 package com.udacity.gradle.builditbigger;
 
+import android.test.AndroidTestCase;
+
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+import rx.schedulers.Schedulers;
+
 /**
  * Created by yehia on 05/04/17.
  */
 
-public class FetchJokesTest {
+public class FetchJokesTest extends AndroidTestCase {
+
+    public void testJoke(){
+        RxEndPoint.getJoke()
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action1<String>() {
+                    @Override
+                    public void call(String joke) {
+                        assert joke != null;
+                        assert !joke.equals("");
+                    }
+                });
+    }
+
 }
--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -9,6 +9,8 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.fci.yehiahd.jokelibrary.JokeActivity;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 
 import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Action1;
@@ -21,6 +23,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AdView mAdView = (AdView) findViewById(R.id.adView);
+        // Create an ad request. Check logcat output for the hashed device ID to
+        // get test ads on a physical device. e.g.
+        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+        mAdView.loadAd(adRequest);
     }
 
     @Override
